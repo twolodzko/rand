b1c79513f03519f025a9750ca442489f1d4ad309
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-03-31 20:53:37 +0200

    Fix test messages

--- test.sh
+++ test.sh
@@ -2,53 +2,53 @@
 set -eu -o pipefail
 
 if [ "$( ./rand main.go | wc -l |  cut -f1 )" -ne 10 ]; then
-    echo "sample didn't return 10 lines"
+    echo "rand didn't return 10 lines"
     exit 1
 fi
 
 if [ "$( ./rand -n 15 main.go | wc -l | cut -f1 )" -ne 15 ]; then
-    echo "sample -n 15 didn't return 15 lines"
+    echo "rand -n 15 didn't return 15 lines"
     exit 1
 fi
 
 if [ "$( ./rand -p 0.9 main.go | wc -l | cut -f1 )" -le 10 ]; then
-    echo "sample -p 0.9 returned <= 10 lines"
+    echo "rand -p 0.9 returned <= 10 lines"
     exit 1
 fi
 
 if [ "$( ./rand go.mod | wc -l | cut -f1 )" -ne "$( cat go.mod | wc -l | cut -f1 )" ]; then
-    echo "sample of small file didn't return all the lines"
+    echo "rand of small file didn't return all the lines"
     exit 1
 fi
 
 if [ "$( ./rand -p 1 go.mod | wc -l | cut -f1 )" -ne "$( cat go.mod | wc -l | cut -f1 )" ]; then
-    echo "sample -p 1 of small file didn't return all the lines"
+    echo "rand -p 1 of small file didn't return all the lines"
     exit 1
 fi
 
 if [ "$( ./rand -l main.go | grep -c -E "\s+[0-9]+" )" -ne 10 ]; then
-    echo "sample -l didn't number the lines"
+    echo "rand -l didn't number the lines"
     exit 1
 fi
 
 if [ "$( cat -n main.go | ./rand -n 20 | uniq | wc -l )" -ne 20 ]; then
-    echo "sample -n 20 didn't return unique lines"
+    echo "rand -n 20 didn't return unique lines"
     exit 1
 fi
 
 result="$( cat -n main.go | ./rand -p 0.5 )"
 if [ "$( echo "$result" | uniq | wc -l )" -ne "$( echo "$result" | wc -l )" ]; then
-    echo "sample -p 0.5 didn't return unique lines"
+    echo "rand -p 0.5 didn't return unique lines"
     exit 1
 fi
 unset result
 
 if [ "$( ./rand -r 42 -n 10 main.go )" != "$( ./rand -r 42 -n 10 main.go )" ]; then
-    echo "fixing random seed didn't lead to identical results for sample -n"
+    echo "fixing random seed didn't lead to identical results for rand -n"
     exit 1
 fi
 
 if [ "$( ./rand -r 42 -p 0.5 main.go )" != "$( ./rand -r 42 -p 0.5 main.go )" ]; then
-    echo "fixing random seed didn't lead to identical results for sample -p"
+    echo "fixing random seed didn't lead to identical results for rand -p"
     exit 1
 fi
